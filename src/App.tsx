53cd272c996db8805874c6959d3a8cdeb595738b
cheonyulin@gmail.com <cheonyulin@gmail.com>
2022-08-07 01:33:43 +0900

    router 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
-import Profile from "components/UI/organisms/Profile";
-import Gnb from "components/UI/organisms/Gnb";
+import { Routes, Route } from "react-router-dom";
+import Nested from "components/templates/Nested";
+import About from "components/pages/About";
+import Projects from "components/pages/Projects";
+import Skills from "components/pages/Skills";
 
 function App() {
   return (
-    <div className="App max-w-screen-xl w-full mx-auto px-4">
-      <Profile />
-      <Gnb />
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Nested />}>
+          <Route path="" element={<About />} />
+          <Route path="projects" element={<Projects />} />
+          <Route path="skills" element={<Skills />} />
+        </Route>
+      </Routes>
     </div>
   );
 }
