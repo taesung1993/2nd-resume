0a468940acd04e6b162bc80ba3e4ec6246be2261
cheonyulin@gmail.com <cheonyulin@gmail.com>
2022-08-07 00:58:03 +0900

    add gnb and profile

--- src/components/UI/organisms/Gnb.tsx
+++ src/components/UI/organisms/Gnb.tsx
@@ -1,3 +1,22 @@
+import TabMenu from "molecules/TabMenu";
+
 export default function Gnb() {
-  return <div>gnb</div>;
+  const tabs = [
+    {
+      title: "ABOUT ME",
+    },
+    {
+      title: "기술 스택",
+    },
+    {
+      title: "프로젝트",
+    },
+  ];
+  return (
+    <section>
+      <nav className="w-full border-t border-b">
+        <TabMenu tabs={tabs} />
+      </nav>
+    </section>
+  );
 }
---
+++ src/components/UI/organisms/Profile.tsx
@@ -0,0 +1,16 @@
+export default function Profile() {
+  return (
+    <section className="flex flex-col md:mb-10 mb-4">
+      <div className="lg:h-80 md:h-72 h-52 bg-gradient-to-r from-cyan-500 to-blue-500"></div>
+      <div className="flex flex-col items-center justify-center lg:-mt-16 md:-mt-12 -mt-9">
+        <div className="lg:w-28 lg:h-28 md:w-24 md:h-24 w-16 h-16 rounded-full bg-black md:mb-4 mb-2"></div>
+        <h1 className="md:text-2xl text-lg font-bold text-default-black md:mb-2 mb-1">
+          윤태성(Mason)
+        </h1>
+        <h3 className="md:text-lg text-sm text-default-500">
+          동료들과 일하며 즐거움을 느끼는 프론트엔드 개발자
+        </h3>
+      </div>
+    </section>
+  );
+}
